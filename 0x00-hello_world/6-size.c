4fa68c7348ac1b8bd2bcdcf576dceb2e42e23075
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-13 14:22:45 -0700

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
  * main - Entry
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	printf("Size of the type char is : %i byte(s):", sizeof(char));
-	printf("Size of the type in is : %i byte(s):", sizeof(int));
-	printf("Size of the type long int is: %i byte(s):", sizeof(long int));
-	printf("Size of long long int is: %i byte(s)", sizeof(long long int));
-	printf("Size of the type float is: %i byte(s)", sizeof(float));
+	printf("Size of the type char is: %i byte(s):\n", sizeof(char));
+	printf("Size of the type in is: %i byte(s):\n", sizeof(int));
+	printf("Size of the type long int is: %i byte(s)\n:", sizeof(long int));
+	printf("Size of long long int is: %i byte(s)\n", sizeof(long long int));
+	printf("Size of the type float is: %i byte(s)\n", sizeof(float));
 	return (0);
 }
 
