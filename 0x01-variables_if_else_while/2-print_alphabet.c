e48cacf6ce05d4c427218daa2d7d9a522a29e1f3
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-14 17:56:58 -0700

    Task 2 first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Function
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	t = tolower(t);
+	putchar(t + "\n");
+	return (0);
+}
