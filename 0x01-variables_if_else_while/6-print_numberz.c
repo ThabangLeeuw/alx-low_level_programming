e85737587847d0d907af76067dfce4bc5a0f769b
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-16 06:44:08 -0700

    task6 first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+
+}
