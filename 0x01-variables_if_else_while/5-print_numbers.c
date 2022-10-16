5a58e5b0ed60b91f4a2590026461e53bc1254eba
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-16 05:47:15 -0700

    Task 5 first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -Function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
