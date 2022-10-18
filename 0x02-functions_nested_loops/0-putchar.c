86910f1829533f9e56147fe6ac0272f2f6e32199
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-18 12:04:33 -0700

    0 third commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main -Function
  * Return: 0
  */
 int main(void)
 {
+	char k = 'putchar';
+
 #include "main.h"
-	printf("_putchar");
+
+	putchar(k);
 	return (0);
 }
