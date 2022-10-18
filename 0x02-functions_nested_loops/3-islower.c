9eb620cdfe9f1ecb12ffe7a72fd3cd8fef57691f
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-18 14:12:31 -0700

    task 3 first commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Tells us how many characters are in lowercase
+ * Return: 0
+ * Written by Imraan ThabangLeeuw
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1)
+	}
+	else
+	{
+		return (0);
+	}
+}
