6be7b8069a45df75c83097075ad10b7678bf66d4
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-19 01:31:34 -0700

    Task 7 first commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - will print the last digit of a number
+ * @p: value that will be printed
+ * Return: last digit
+ */
+int print_last_digit(int p)
+{
+	return (p % 10);
+}
