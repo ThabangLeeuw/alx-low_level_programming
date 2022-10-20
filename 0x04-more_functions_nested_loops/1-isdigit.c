3948e221a122a77553c6d947538fc850baffe304
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-20 15:47:27 -0700

    tasl 1 first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - prints digits and return 1 if digits and 0 if otherwise
+ * @c: returns 1 if digits and 0 if otherwise
+ * Return: same as above
+ */
+int _isdigit(int c)
+{
+	if (c >= 0  && c <= 9)
+		return (1);
+	else
+		return (0);
+}
