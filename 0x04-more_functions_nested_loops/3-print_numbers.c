be1af0b2ec61627a3c936e3633452df726d01fb5
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-20 16:31:48 -0700

    Task 3 first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0 to 9
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int g
+
+	while (g >= 0 && g <= 9)
+	{
+		_putchar(g);
+		g++;
+
+	}
+}
