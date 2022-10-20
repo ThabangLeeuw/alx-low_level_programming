1457ff56d088bbdcda230327d311a86918de0b84
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-20 16:21:58 -0700

    Task 2 first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a multiplied by b
+ */
+int mul(int a, int b)
+{
+	res = a * b;
+	putchar (res);
+}
