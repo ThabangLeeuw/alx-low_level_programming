cee8fbaaf08f4e058ab73bae31cedf31c9e761e2
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-20 16:13:52 -0700

    Task 1 seventh commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,16 +6,12 @@
  */
 int _isdigit(int c)
 {
-	while (c >= 48 && c <= 57)
+	if (c >= 48  &&  c <= 57)
 	{
-		if (c >= 48  &&  c <= 57)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-		c++;
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
