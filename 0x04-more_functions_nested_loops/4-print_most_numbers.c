61bd47801cf230bf3b13b150085587af7ae0930f
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-21 12:50:03 -0700

    Task 4 first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that will print from 0 -9 and skip 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+
+		if ((p != 2) && (p != 4))
+		{
+			_putchar(p + '0');
+		}
+		else
+		{
+			continue;
+		}
+
+	}
+}
