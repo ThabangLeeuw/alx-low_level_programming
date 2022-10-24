6d9ee8876853e05e82ee40c9cc066adbb5ad1322
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-24 11:29:32 -0700

    Task 2 first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - _strlen function
+ * @s: declared variable
+ * Return: returning the length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+
+}
