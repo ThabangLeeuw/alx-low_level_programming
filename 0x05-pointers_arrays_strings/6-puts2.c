8706a44b26b8a6d481743f99bdc7186a39441895
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-24 15:26:11 -0700

    task 6 first commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - function used to print every other char
+ * @str: declared variabled
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] >= 0; i++)
+	{
+
+		if (str[i] == '\0')
+		{
+			putchar('\n')
+			break;
+		}
+
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+}
