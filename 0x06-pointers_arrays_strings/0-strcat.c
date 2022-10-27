bba93b5ac44c0d78e5d8fbb01b748d4f2c5320ef
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-27 14:23:37 -0700

    task 0 first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - Function that will concatenate two strings
+ * @dest: string that will be appended to
+ * @src: string we are appending from
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	return (dest);
+}
