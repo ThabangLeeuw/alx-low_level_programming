c4d29cd4560a55802881a80abb4341bdb288820f
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-10-31 05:18:21 -0700

    task 0 first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
- * @i - given parameter
  * Return: 0
+ * positive_or_negative - calling this function
  */
 int main(void)
 {
