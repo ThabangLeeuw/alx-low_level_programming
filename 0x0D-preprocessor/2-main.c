ee49e5351cfce657946f48a82983934daf5ae847
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-11-14 20:49:26 +0200

    Task 2 commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*
- * main-Function name used that will print the name of this file
+/**
+ * main - Function name used that will print the name of this file
  * Return: 0
  */
 int main(void)
 {
 
-	printf("File name: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
