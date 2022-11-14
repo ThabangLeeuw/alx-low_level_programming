43ef5200816b0f8047550f68a43f3c74740e49e6
ThabangLeeuw <thabangleeuw97@gmail.com>
2022-11-14 21:04:13 +0200

    Task 3 second commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,6 @@
 #ifndef function_like_macro
 #define function_like_macro
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#define ABS(x)
-/**
- * main - function that will return the absolute value of x
- * Return: Always 0
- */
-int main(void)
-{
-	int x;
 
-	printf("%i\n", ABS(x));
-	return (0);
-}
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+
 #endif
